0566faa4a7cd74747435881bf8197c01379c5c78
wuphilip0427 <wu.philip0427@gmail.com>
2021-11-08 10:10:26 +0800

    use edge as default

--- app_run.py
+++ app_run.py
@@ -2,7 +2,8 @@ import os, webbrowser
 from threading import Timer
 
 def open_browser():
-      webbrowser.open_new('http://127.0.0.1:5000/')
+      webbrowser.register('edge', None, webbrowser.BackgroundBrowser("C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"))
+      webbrowser.get('edge').open('http://127.0.0.1:5000/')
 
 if __name__=="__main__":
     Timer(1, open_browser).start()
